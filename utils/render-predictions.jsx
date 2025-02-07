834979518b5dd2fa7ad9dba1ac4c823c67b09579
Deva9754 <deva.sahu2001@gmail.com>
2025-02-07 14:38:25 +0530

    fix:object detectionerror fixed

--- utils/render-predictions.jsx
+++ utils/render-predictions.jsx
@@ -19,19 +19,7 @@
 
 // //fill the color
 // ctx.fillStyle = `rgba(255, 0, 0, ${isPerson ? 0.2 : 0})`;
-// ctx.fillRect(x, y, width, height);
 
-// //Draw the Label Background.
-// ctx.fillStyle = isPerson ? "#FF0000" : "#00FFFF";
-// const textWidth = ctx.measureText(prediction.class).width;
-// const textHeight = parseInt(font, 10); //base 10
-// ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
-
-// ctx.fillStyle = "#000000";
-// ctx.fillText(prediction.class, x, y);
-
-//     })
-// }
 
 import {throttle} from "lodash";
 
